81c2255fa090467b313eff282a73e1de76387148
MrRedByte <81798180+MrRedByte@users.noreply.github.com>
2022-10-26 16:11:03 +0000

    Add files via upload

---
+++ journalismporiect.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cidul</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="journalismproject.css" />
+    <script type="text/javascript" src="journalismproject.js" defer></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="navbar">
+        <nav>
+          <ul>
+            <li>Feik News<sup style="font-size: 18px">&#8482;&copy;</sup></li>
+          </ul>
+        </nav>
+        <div id="progress-bar"></div>
+      </div>
+      <article>
+        <h1>Cidul: un cavaler în Spania medievală</h1>
+        <div>
+          <p>
+            Rodrigo Díaz de Vivar (aproximativ 1043 – 10 iulie 1099) a fost un
+            cavaler și un conducător de război castilian în Spania medievală.
+            Luptând cu armatele musulmane în timpul vieții sale, el a câștigat
+            onorificiul arab al-sīd, care va evolua în El Cid („domnul”) și
+            porecla spaniolă El Campeador („curajosul”). Díaz de Vivar a devenit
+            bine cunoscut pentru serviciul său în armatele conducătorilor
+            creștini. După moartea sa, El Cid a devenit celebrul erou național
+            al Spaniei și protagonistul celui mai important poem epic medieval
+            spaniol,
+            <i>El Cantar de mio Cid</i>, care îl prezintă ca fiind cavalerul
+            medieval ideal: puternic, viteaz, loial, drept și pios.
+          </p>
+          <h2>Proveniența numelui</h2>
+          <p>
+            Rodrigo Díaz a fost recunoscut cu titlul onorific de „Campeador”
+            de-a lungul vieții. Titlul „Campeador” provine din latinescul
+            „Campidoctor”, însemnând literal „Profesor al câmpului”, dar poate
+            fi tradus ca „Maestru al câmpului de luptă”. Epitetul de „El Cid”,
+            însemna <i>Domnul</i> probabil din originalul arab, (arabă:
+            السَّيِّد, romanizat: al-Sayyid) și era un titlu dat altor lideri
+            creștini.
+          </p>
+          <h2>Rolul lui Díaz de Vivar</h2>
+          <p>
+            Regele Alfonso al VI-lea, l-a ales pe Cid să strangă
+            <i>parias</i> (taxe/impozite lunare) de la toate cetațiile musulmane
+            care erau cucerite de spanioli. În urma exilului, taxele erau
+            strânse atat pentru întărirea armatei, cât și pentru a evita
+            revoltele interne ale arabilor, acesta oferind a cincea parte din
+            totalul taxelor arabilor din cetățile cucerite.
+          </p>
+          <h2>Exilul Cidului</h2>
+          <p>
+            Rodrigo Díaz de Vivar era un nobil, cinstit, iubit de toți. Însa
+            prin noblețea sa a starnit invidia a doi indivizi care l-au acuzat
+            pe nedrept că ar fi pastrat o parte din banii colectați prin taxe.
+            Contele García Ordóñez, un rival al acestuia l-a acuzat pe Cid în
+            fața regelui că pastrează pentru sine o parte din taxe. Regele l-a
+            exilat pe Cid, iar acesta s-a despartit cu greu de sotia sa Jimena
+            si de cele doua fiice ale lor și le-a lasat în grija abatelui
+            manastirii San Pedro.
+          </p>
+          <h2>Cuceririle Cidului</h2>
+          <p>
+            Insotit de oamenii săi, Cidul a hotarât să cucerească vechile
+            teritorii ale Spaniei, care erau acum conduse de către regi
+            musulmani. Astfel ei au cucerit castelul Alcocer dupa o asediere de
+            cincisprezece săptămâni. Cei din castel i-au cerut ajutor regelui
+            Tamin, din Valencia, care a trimis o armata de 3000 de soldați să se
+            lupte cu armata Cidului, care avea doar de 200 de soldați. După
+            patru saptamani de asediu, Cidul împreuna cu oamenii săi ies pe
+            campul de luptă și îl inving pe Tamin și oastea sa.
+          </p>
+          <br />
+          <img
+            src="drumul.jpg"
+            alt="Imagine cu drumul Cidului"
+            srcset=""
+            style="width: 100%; height: auto"
+          />
+          <br />
+          <h2>Reîntalnirea cu familia sa</h2>
+          <p>
+            După multe cuceriri, Cidul a cucerit și orasul Valencia. Acesta l-a
+            trimis pe locotenetul Minava Álvar Fanez dăruindu-i 30 de cai de
+            rasă nobilă, rugându-l sa le lase pe sotia sa, Jimena, și cele două
+            fiice ale lor sa vină în Valencia. Langă regele Alfonso erau doi
+            tineri, care auzind cât de bogat era Cidul au hotarât să se
+            căsătorească cu fiicele acestuia. Pană la urmă regele Alfonso se
+            hotaraste să lase familia Cidului să plece în Valencia, unde erau
+            fericiti la reîntalnea lor după o lungă perioadă de timp.
+          </p>
+          <h2>Iertarea regelui</h2>
+          <p>
+            Regele Marocului, Yusuf a vrut să cucerească Valencia. Cidul s-a
+            luptat el însuși cu regele Yusuf, l-a înfrânt și l-a luat prizonier,
+            astfel Cidul a devenit mai faimos. Când regele Alfonso a primit
+            darurile de la Cid, din nou cei doi tineri nobili l-au rugat să-i
+            lase să se căsătoreasca cu fetele Cidului. Regele Alfonso văzând
+            loialitatea Cidului, îi trimite acestuia vorba să se întâlnească la
+            curtea din Toled, unde îi ceru Cidului mâna celor două fete ale sale
+            pentru fii contelui González. Cidul era bucuros să-și vada din nou
+            regele, dar în acelasi timp era suspicios pe cei doi tineri care se
+            căsătoriseră cu fetele sale.
+          </p>
+          <h2>Ticăloșia indivizilor</h2>
+          <p>
+            Vreme de doi ani, cei doi fii de conte, Diego și Fernando González,
+            au dus o viață lipsită de griji in Valencia, dar reputația lor
+            devenea din ce in ce mai precară de la o zi la alta. Aceștia îi
+            ceruseră Cidului să le dea voie să mearga în Castilia împreună cu
+            soțiile lor. Ei au fost însoțiți de nepotul Cidului, Felez Munoz, ca
+            să aibe grijă de fiicele sale. Când au ajuns in Castilia s-au oprit
+            într-o pădure, unde cei doi le-au dezbrăcat pe soțiile lor, le-au
+            legat de copaci și le-au biciuit, crezând astfel că vor scapă de
+            sentimentul de umilintă. Felez Munoz le scapă pe fetele Cidului și
+            le duce la tătal lor. Fiicele lui Cidului, Cristina Rodríguez și
+            María, s-au căsătorit în familii nobile. Cristina s-a căsătorit cu
+            Ramiro, Lord de Monzón, nepotul lui García Sánchez al III-lea al
+            Navarrei. Propriul ei fiu, nepotul lui El Cid, va fi ales ca rege pe
+            tronul Navarrei ca regele García Ramírez. Cealaltă fiică, María, se
+            spune că s-a căsătorit cu un prinț al Aragonului.
+          </p>
+          <h2>Surse</h2>
+          <ul>
+            <li>
+              <a href="https://en.wikipedia.org/wiki/Le_Cid"
+                >Le Cid - Wikipedia</a
+              >
+            </li>
+            <li>
+              <a href="https://en.wikipedia.org/wiki/El_Cid"
+                >El Cid - Wikipedia</a
+              >
+            </li>
+            <li>
+              <a href="https://en.wikipedia.org/wiki/Cantar_de_mio_Cid"
+                >Cantar de mio Cid - Wikipedia</a
+              >
+            </li>
+            <li>
+              <a href="https://en.caminodelcid.org/way-cid/cartographic-viewer/"
+                >Drumul Cidului</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.encyclopedia.com/arts/culture-magazines/poem-cid"
+                >Poem of the Cid</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://discover.hubpages.com/literature/El-Cid-Spains-epic-hero-and-poem"
+                >El Cid - Spain's epic hero and poem</a
+              >
+            </li>
+            <li>
+              <a href="https://en.caminodelcid.org/cid-history-legend/song-cid/"
+                >The greatest Hispanic epic poem</a
+              >
+            </li>
+            <li>
+              <a href="https://www.youtube.com/embed/Wly1baoiJaQ"
+                >The Song of El Cid</a
+              >
+            </li>
+          </ul>
+        </div>
+      </article>
+    </div>
+  </body>
+</html>
